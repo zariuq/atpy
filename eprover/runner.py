c6388159c1919f94490a0ee246ded46198b2a0ca
Jan Jakubuv <jakubuv@gmail.com>
2019-02-22 13:15:22 +0100

    Generalized limit

--- eprover/runner.py
+++ eprover/runner.py
@@ -8,22 +8,27 @@ E_BIN = "eprover"
 E_ARGS = "%s -p --resources-info --memory-limit=20480 --print-statistics --tstp-format"
 E_DEFAULTS = "-s"
 
+LIMIT = {
+   "T": lambda x: "--soft-cpu-limit=%s --cpu-limit=%s" % (x,int(x)+1),
+   "P": lambda x: "--processed-set-limit=%s" % x,
+   "C": lambda x: "--processed-clauses-limit=%s" % x,
+   "G": lambda x: "--generated-limit=%s" % x
+}
+
 #E_ARGS = "%s -s -p --free-numbers --resources-info --memory-limit=1024 --print-statistics --tstp-format --training-examples=3"
 #E_ARGS = "--cpu-limit=%s -s -p --free-numbers --resources-info --memory-limit=1024 --print-statistics --tstp-format --training-examples=3"
 
 def cmd(f_problem, proto, limit, ebinary=None, eargs=None):
+   "Limit format is: 'Tnnn-Pnnn-Cnnn-Gnnn' or a subexpression, even like 'Tnnn'."
    ebinary = ebinary if ebinary else E_BIN
    eargs = eargs if eargs else E_DEFAULTS
-   if isinstance(limit, int):
-      limit = "--soft-cpu-limit=%s --cpu-limit=%s" % (limit, limit+1)
-   elif isinstance(limit, str) and "-" in limit:
-      # otherwise limit format "Txxx-Cyyy"
-      (t,p) = limit.split("-")
-      (t,p) = (t[1:], p[1:])
-      (t,p) = (int(t), int(p))
-      limit = "--soft-cpu-limit=%s --cpu-limit=%s --processed-set-limit=%s" % (t,t+1,p)
-   else:
+   limit = "T%s" % limit if isinstance(limit,int) else limit
+
+   try:
+      limit = [LIMIT[x[0]](x[1:]) for x in limit.split("-")]
+   except:
       raise Exception("atpy.eprover.runner: Unknown E limit for eprover.runner (%s)"%limit)
+   limit = " ".join(limit)
 
    estatic = E_ARGS % "%s %s" % (limit, eargs)
    return "%s %s %s %s %s" % (PERF,ebinary,estatic,proto,f_problem)
