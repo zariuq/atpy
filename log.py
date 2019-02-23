0df8efdaa0417e805fcb72656580a00de1069188
Jan Jakubuv <jakubuv@gmail.com>
2019-02-23 22:10:43 +0100

    Automatic runlog in the current dir

--- log.py
+++ log.py
@@ -1,7 +1,15 @@
 from datetime import datetime
 
-def msg(msg, first=[]):
+def msg(msg, first=[], prefix="~~~ atpy runlog ~~~ "):
+   now = datetime.now()
    if not first:
-      first.append(datetime.now())
-   print "[%s] %s" % (datetime.now()-first[0], msg)
+      first.append(now)
+   
+   msg = "[%s] %s" % (now-first[0], msg)
+   print msg
+   if prefix:
+      f = file(prefix+str(first[0]),"a")
+      f.write(msg+"\n")
+      f.flush()
+      f.close()
 
