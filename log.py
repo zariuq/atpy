d7e1952cb9477cc5bacd1f933b354feb577ef525
Jan Jakubuv <jakubuv@gmail.com>
2019-02-24 20:48:36 +0100

    Use atpy.log for expres.dump

--- log.py
+++ log.py
@@ -3,7 +3,7 @@ from sys import argv, exc_info
 import sys
 import atexit
 
-PREFIX = "~~~ atpy runlog ~~~ "
+PREFIX = "~~~atpy~log~~~"
 
 def terminating(cache):
    msg("Terminating.")
@@ -17,19 +17,22 @@ def start(intro, config=None, script=""):
    intro = "%s\n\n   %s\n" % (intro, config)
    msg(intro, script=script)
 
-def msg(msg, cache=[], script=""):
+def msg(msg, cache=[], script="", timestamp=True):
    now = datetime.now()
    if not cache:
       script = argv[0] if argv and not script else script
       cache.append(now)
-      cache.append("%s%s ~~~ %s" % (PREFIX, script.lstrip("./").replace("/","+"), now))
+      cache.append(("%s%s~~~%s" % (PREFIX, script.lstrip("./").replace("/","+"), now)).replace(" ","~"))
       atexit.register(terminating, cache)
    
-   msg = "[%s] %s" % (now-cache[0], msg)
+   msg = ("[%s] %s" % (now-cache[0], msg)) if timestamp else msg
    print msg
    if PREFIX:
       f = file(cache[1],"a")
       f.write(msg+"\n")
       f.flush()
       f.close()
+
+def text(msg0=""):
+   msg(msg0, timestamp=False)
 
